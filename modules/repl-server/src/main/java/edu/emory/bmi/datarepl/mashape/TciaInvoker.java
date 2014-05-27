ee9ef4049557f1fe6687d048d5d9909a7c136552
pradeeban <kk.pradeeban@gmail.com>
2014-05-27 13:46:31 +0100

    Including the test class, and including all the mashape tcia methods.

--- modules/repl-server/src/main/java/edu/emory/bmi/datarepl/mashape/TciaInvoker.java
+++ modules/repl-server/src/main/java/edu/emory/bmi/datarepl/mashape/TciaInvoker.java
@@ -9,7 +9,6 @@
 package edu.emory.bmi.datarepl.mashape;
 
 import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import edu.emory.bmi.datarepl.core.CommonConstants;
@@ -22,12 +21,101 @@ import org.apache.logging.log4j.Logger;
 public class TciaInvoker {
     private static Logger logger = LogManager.getLogger(TciaInvoker.class.getName());
 
+    /**
+     * /GET the collection values
+     * @return collection values
+     * @throws UnirestException
+     */
     public static HttpResponse getCollectionValues() throws UnirestException {
-            HttpResponse<JsonNode> request = Unirest.get("https://tcia.p.mashape.com/getCollectionValues?format=%3Cformat%3E")
-                    .header("X-Mashape-Authorization", CommonConstants.MASHAPE_AUTHORIZATION)
-                    .header("api_key", CommonConstants.API_KEY)
-                    .asJson();
-            return request;
+        return Unirest.get("https://tcia.p.mashape.com/getCollectionValues?format=%3Cformat%3E")
+                .header("X-Mashape-Authorization", CommonConstants.MASHAPE_AUTHORIZATION)
+                .header("api_key", CommonConstants.API_KEY)
+                .asJson();
     }
 
+    /**
+     * /GET the values of the body parts
+     * @return bodyPartValues
+     * @throws UnirestException
+     */
+    public static HttpResponse getBodyPartValues() throws UnirestException {
+        return Unirest.get("https://tcia.p.mashape.com/getBodyPartValues?format=%3Cformat%3E&Collection=%3CCollection%3E&BodyPartExamined=%3CBodyPartExamined%3E&Modality=%3CModality%3E")
+                .header("X-Mashape-Authorization", CommonConstants.MASHAPE_AUTHORIZATION)
+                .header("api_key", CommonConstants.API_KEY)
+                .asJson();
+    }
+
+    /**
+     * /GET Manufacturer values
+     * @return manufacturer values
+     * @throws UnirestException
+     */
+    public static HttpResponse getManufacturerValues() throws UnirestException {
+        return Unirest.get("https://tcia.p.mashape.com/getManufacturerValues?format=%3Cformat%3E&Collection=%3CCollection%3E&BodyPartExamined=%3CBodyPartExamined%3E&Modality=%3CModality%3E")
+                .header("X-Mashape-Authorization", CommonConstants.MASHAPE_AUTHORIZATION)
+                .header("api_key", CommonConstants.API_KEY)
+                .asJson();
+    }
+
+    /**
+     * /GET Modality values
+     * @return modality values
+     * @throws UnirestException
+     */
+    public static HttpResponse getModalityValues() throws UnirestException {
+        return Unirest.get("https://tcia.p.mashape.com/getModalityValues?Collection=%3CCollection%3E&BodyPartExamined=%3CBodyPartExamined%3E&Modality=%3CModality%3E&format=%3Cformat%3E")
+                .header("X-Mashape-Authorization", CommonConstants.MASHAPE_AUTHORIZATION)
+                .header("api_key", CommonConstants.API_KEY)
+                .asJson();
+    }
+
+    /**
+     * /GET patient
+     * @return patient
+     * @throws UnirestException
+     */
+    public static HttpResponse getPatient() throws UnirestException {
+        return  Unirest.get("https://tcia.p.mashape.com/getPatient?format=%3Cformat%3E&Collection=%3CCollection%3E")
+                .header("X-Mashape-Authorization", CommonConstants.MASHAPE_AUTHORIZATION)
+                .header("api_key", CommonConstants.API_KEY)
+                .asJson();
+    }
+
+    /**
+     * /GET patientStudy
+     * @return patientStudy
+     * @throws UnirestException
+     */
+    public static HttpResponse getPatientStudy() throws UnirestException {
+        return Unirest.get("https://tcia.p.mashape.com/getPatientStudy?format=%3Cformat%3E&Collection=%3CCollection%3E&PatientID=%3CPatientID%3E&StudyInstanceUID=%3CStudyInstanceUID%3E")
+                .header("X-Mashape-Authorization", CommonConstants.MASHAPE_AUTHORIZATION)
+                .header("api_key", CommonConstants.API_KEY)
+                .asJson();
+    }
+
+    /**
+     * /GET series
+     * @return series
+     * @throws UnirestException
+     */
+    public static HttpResponse getSeries() throws UnirestException {
+        return Unirest.get("https://tcia.p.mashape.com/getSeries?format=%3Cformat%3E&Collection=%3CCollection%3E&PatientID=%3CPatientID%3E&StudyInstanceUID=%3CStudyInstanceUID%3E&Modality=%3CModality%3E")
+                .header("X-Mashape-Authorization", CommonConstants.MASHAPE_AUTHORIZATION)
+                .header("api_key", CommonConstants.API_KEY)
+                .asJson();
+    }
+
+    /**
+     * /GET Image
+     * @param SeriesInstanceUID, instance ID of the series
+     * @return image as a zip
+     * @throws UnirestException
+     */
+    public static HttpResponse getImage(String SeriesInstanceUID) throws UnirestException {
+//        Unirest.get("https://tcia.p.mashape.com/getImage?SeriesInstanceUID=%3CSeriesInstanceUID%3E")
+        return Unirest.get("https://tcia.p.mashape.com/getImage?SeriesInstanceUID="+SeriesInstanceUID)
+                .header("X-Mashape-Authorization", CommonConstants.MASHAPE_AUTHORIZATION)
+                .header("api_key", CommonConstants.API_KEY)
+                .asBinary();
+    }
 }
