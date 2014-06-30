39863c95793da85eaeafe467e7fae70a97f4c85a
pradeeban <kk.pradeeban@gmail.com>
2014-06-30 23:08:12 +0100

    Header update.

--- modules/repl-server/src/main/java/edu/emory/bmi/datarepl/tcia/DataProSpecs.java
+++ modules/repl-server/src/main/java/edu/emory/bmi/datarepl/tcia/DataProSpecs.java
@@ -1,3 +1,11 @@
+/*
+ * Title:        Data Replication Server
+ * Description:  Data Replication / Synchronization Tools.
+ * Licence:      Apache License Version 2.0 - http://www.apache.org/licenses/
+ *
+ * Copyright (c) 2014, Pradeeban Kathiravelu <pradeeban.kathiravelu@tecnico.ulisboa.pt>
+ */
+
 package edu.emory.bmi.datarepl.tcia;
 
 import edu.emory.bmi.datarepl.constants.InfConstants;
